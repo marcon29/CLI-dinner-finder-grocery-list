9e9c9d41643d828cef9a5dacdf1bb67c1bd49eaa
Mark Stabler <mark@markstabler.com>
2020-04-06 17:47:53 +0000

    refactored CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,4 @@
 class CLI
-  # need to exit at any time
-
-  # need main menu
   def main_menu
     puts "What would you like to do? (enter number)"
     puts "1. Add recipe"
@@ -64,6 +61,22 @@ class CLI
     end
   end
 
+  def browse(name)
+    Category.new(name) if !Category.all_names.include?(name)
+    category = Category.find_by_name(name)
+    selection = category.menu
+
+    if selection == "bad input"
+      browse(name)
+    elsif category.parent == "main"
+      browse(selection) if selection != "back"
+    elsif category.parent == "meal type"
+      selection == "back" ? browse(category.parent) : browse(selection)
+    else
+      selection == "back" ? browse(category.parent) : get_recipe(selection)
+    end
+  end
+
   def get_recipe(input)
     begin
       Recipe.new(input)
@@ -77,8 +90,7 @@ class CLI
   end
 
   def view_recipes_menu_option
-    if Recipe.all.count == 0
-      puts "You haven't added any recipes yet."
+    if no_recipes?
     else
       puts "Here are all your current recipes."
       puts Recipe.all_names
@@ -86,10 +98,10 @@ class CLI
   end
 
   def delete_recipe_menu_option
-    count = Recipe.all.count
-    if count == 0
-      puts "You haven't added any recipes yet."
+    if no_recipes?
     else
+      count = Recipe.all.count
+
       puts "Which recipe would you like to remove? (enter 1-#{count+2})"
       Recipe.all_names.each_with_index { |r, i| puts "#{i+1}. #{r}" }
       puts "#{count+1}. Delete ALL recipes."
@@ -114,8 +126,7 @@ class CLI
   end
 
   def grocery_list_menu_option
-    if Recipe.all.count == 0
-      puts "You haven't added any recipes yet."
+    if no_recipes?
     else
       Recipe.all.each do |recipe|
         puts recipe.name
@@ -129,29 +140,10 @@ class CLI
     input.split.collect(&:capitalize).join(' ')
   end
 
-
-
-
-
-  # this will control the menu flow for categories
-  # starts w/ name = "meal type"
-  def browse(name)
-    Category.new(name) if !Category.all_names.include?(name)
-    category = Category.find_by_name(name)
-    selection = category.menu
-
-    if selection == "bad input"
-      browse(name)
-    elsif category.parent == "main"
-      browse(selection) if selection != "back"
-    elsif category.parent == "meal type"
-      selection == "back" ? browse(category.parent) : browse(selection)
-    else
-      selection == "back" ? browse(category.parent) : get_recipe(selection)
+  def no_recipes?
+    if Recipe.all.count == 0
+      puts "You haven't added any recipes yet."
+      true
     end
   end
-
-
-
-
 end
